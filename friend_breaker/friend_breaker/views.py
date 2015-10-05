0cf89a911d638f522f2a28d46ccab110cf7fa4ca
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-10-05 15:34:28 -0700

    ignore ties

--- friend_breaker/friend_breaker/views.py
+++ friend_breaker/friend_breaker/views.py
@@ -52,35 +52,48 @@ def _build_context(client, athlete_id):
         for segment in segment_efforts:
             mysegments[segment.segment.id] = segment.segment  # save to db
 
-    count = 0  # for testing, limit segments
+    # count = 0  # for testing, limit segments
 
     # check if segment leaderboard contains any friends
     for key, segment in mysegments.iteritems():
         leaderboard = client.get_segment_leaderboard(key, following=True).entries   # API call (possibly lots, depends on number of segments)
 
         # limit the segments while testing
-        count += 1
-        if count > 10:
-            break
+        # count += 1
+        # if count > 10:
+        #     break
 
         # get friend with time < athlete time
-        for entry in leaderboard:
+        for i, entry in enumerate(leaderboard):
             if entry.athlete_id == athlete_id:
                 me = entry
-                index = leaderboard.index(me)
-                if index > 0:
-                    other = leaderboard[index - 1]
-                    segments[segment.name] = {}
-                    segments[segment.name]['segment_name'] = segment.name
-                    segments[segment.name]['segment_id'] = segment.id
-                    segments[segment.name]['challenger_name'] = other.athlete_name
-                    segments[segment.name]['challenger_id'] = other.athlete_id
-                    segments[segment.name]['my_pr_id'] = me.activity_id
-                    segments[segment.name]['their_pr_id'] = other.activity_id
-                    segments[segment.name]['my_elapsed_time'] = str(me.elapsed_time)
-                    segments[segment.name]['their_elapsed_time'] = str(other.elapsed_time)
-                    segments[segment.name]['time_difference'] = str(me.elapsed_time - other.elapsed_time)
-                    segments[segment.name]['distance'] = str(unithelper.miles(segment.distance))
+
+                if i == 0:
+                    # I'm already the winner!
+                    break
+
+                j = 1
+                other = leaderboard[i - j]
+                while other.elapsed_time == me.elapsed_time and j <= i:
+                    # check for ties, compare each entry from i to zero (possibly)
+                    j += 1
+                    other = leaderboard[i - j]
+                if other.elapsed_time == me.elapsed_time:
+                    # if they're still tied at the end of the loop, I don't want to see it
+                    break
+
+                segments[segment.name] = {}
+                segments[segment.name]['segment_name'] = segment.name
+                segments[segment.name]['segment_id'] = segment.id
+                segments[segment.name]['challenger_name'] = other.athlete_name
+                segments[segment.name]['challenger_id'] = other.athlete_id
+                segments[segment.name]['my_pr_id'] = me.activity_id
+                segments[segment.name]['their_pr_id'] = other.activity_id
+                segments[segment.name]['my_elapsed_time'] = str(me.elapsed_time)
+                segments[segment.name]['their_elapsed_time'] = str(other.elapsed_time)
+                segments[segment.name]['time_difference'] = str(me.elapsed_time - other.elapsed_time)
+                segments[segment.name]['distance'] = str(unithelper.miles(segment.distance))
+                break  # we already found my entry, why keep looking through the list?
 
     context['segments'] = segments
     context['all_segments'] = mysegments
